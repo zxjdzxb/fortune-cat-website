f7e443c05c3cadeaaed02606bfa781a617bbefff
周兴杰 <1660154581@qq.com>
2022-07-28 09:32:35 +0800

    deploy

---
+++ precache-manifest.e2fdb3aa6a54e9a5bd4d62526060cbbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a4b16628caa83353eb",
+    "url": "../fortune-cat-website/css/app.b91ccb4f.css"
+  },
+  {
+    "revision": "b6c3d1a3f68481d49f7946ad58ea279b",
+    "url": "../fortune-cat-website/index.html"
+  },
+  {
+    "revision": "a0a4b16628caa83353eb",
+    "url": "../fortune-cat-website/js/app.3619134c.js"
+  },
+  {
+    "revision": "fa57d0845cffb13fa078",
+    "url": "../fortune-cat-website/js/chunk-vendors.b8da09d9.js"
+  },
+  {
+    "revision": "f6e8b7623d419c48444a32782369a814",
+    "url": "../fortune-cat-website/manifest.json"
+  },
+  {
+    "revision": "a69795ee393a0b14bc4e8bf90bb3b433",
+    "url": "../fortune-cat-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "../fortune-cat-website/robots.txt"
+  }
+]);
